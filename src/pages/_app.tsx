9d75d2a7931e4944f717e8de404fb62d897e18ba
Himanshu Chhatpar <himanshu@shelfpay.in>
2024-04-07 17:41:53 +0530

    feat: Added button component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,17 @@ import { Inter } from "next/font/google";
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
+import { Button } from "@/components/atoms/Button";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <main className={`font-sans ${inter.variable}`}>
-      <Component {...pageProps} />
+      <Button variant="default">Button</Button>
     </main>
   );
 };
