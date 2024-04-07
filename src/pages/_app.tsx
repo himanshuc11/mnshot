d2b351f6d497a697d9b96cd131e95bb01fb2e9f2
Himanshu Chhatpar <himanshu@shelfpay.in>
2024-04-07 18:10:25 +0530

    feat: Added Input box

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { api } from "@/utils/api";
 import "@/styles/globals.css";
 import { Button } from "@/components/atoms/Button";
 import { Checkbox } from "@/components/atoms/Checkbox";
+import { Input } from "@/components/atoms/Input";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -17,6 +18,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     <main className={`font-sans ${inter.variable}`}>
       <Button variant="default">Button</Button>
       <Checkbox id="terms1" />
+      <Input type="email" placeholder="Email" />
     </main>
   );
 };
