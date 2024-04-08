901b60cba6b101b2d52114cc44ab14599cbcafad
Himanshu Chhatpar <himanshu@shelfpay.in>
2024-04-08 20:54:54 +0530

    feat: Added email password validation

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -5,7 +5,55 @@ import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { TRPCError } from '@trpc/server';
 import otpGenerator from "otp-generator"
 
+
+
 export const postRouter = createTRPCRouter({
+    loginUser: publicProcedure
+                .input(z.object({
+                  emailId: z.string(),
+                  password: z.string()
+                })).mutation(async ({ input }) => {
+                  const emailId = input.emailId;
+                  const emailData = await prisma.userEmail.findUnique({
+                    where: {
+                     email: emailId
+                    },
+                  });
+
+                  if(emailData === null) {
+                    throw new TRPCError({
+                      code: "CONFLICT",
+                      message: 'Please Sign up before login',
+                    });
+                  }
+                  
+                  const userId = emailData.userId;
+                  const userData = await prisma.user.findUnique({
+                    where: {
+                      id: userId
+                    }
+                  });
+                  const hashedPassword = userData?.password;
+                  if(!hashedPassword) {
+                    throw new TRPCError({
+                      code: "NOT_FOUND",
+                      message: "User not found"
+                    })
+                  }
+                  const isPasswordSame = passwordManager.isHashSame(input.password, hashedPassword);
+                  if(isPasswordSame) {
+                    return {
+                      message: "Login"
+                    }
+                  }
+                  else {
+                    return {
+                      message: "Email Password mismatch"
+                    }
+                  }
+
+                }),
+
 
     createUser: publicProcedure
     .input(z.object({
